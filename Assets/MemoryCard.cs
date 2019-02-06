fbc3d0ba5c4d39f3f23eab28c39e22feb9afc048
optimizasean <optimizasean@gmail.com>
2019-02-06 12:32:46 -1000

    Added card scripts to move and randomize as well as spawn

---
+++ Assets/MemoryCard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController controller;
+
+    private int _id;
+    
+    public int id {
+        get { return _id;}
+    }
+
+    public void setCard(int id, Sprite image) {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void OnMouseDown() {
+        if (cardBack.activeSelf) {
+            cardBack.SetActive(false);
+        }
+    }
+}
