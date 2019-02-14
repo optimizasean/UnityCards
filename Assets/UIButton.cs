b9dd7572b85b5f8280b01c85d0b83eb6ba45e4ee
optimizasean <optimizasean@gmail.com>
2019-02-13 15:06:13 -1000

    Added reset button, game complete

---
+++ Assets/UIButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButton : MonoBehaviour {
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+    public Color highlightColor = Color.cyan;
+
+    public void OnMouseEnter() {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null) {
+            sprite.color = highlightColor;
+        }
+    }
+
+    public void OnMouseExit() {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null) {
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown() {
+        transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+    }
+
+    public void OnMouseUp() {
+        transform.localScale = new Vector3(0.19f, 0.19f, 0.19f);
+        if (targetObject != null) {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
